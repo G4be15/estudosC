67db1da2d1bac819eb581d3aeb1c507a7cfccc0a
Gabriel <gabrielalvesasevdo@gmail.com>
2025-06-02 18:22:52 -0300

    teste1

--- teste1.c
+++ teste1.c
@@ -7,6 +7,19 @@ int main(){
     
     printf("ola, me informe seu nome por favor \n");
     scanf("%s", nome);
+    printf("quantos anos você tem?\n");
+    scanf("%d", &idade);
+    printf("ola %s, você tem %d anos\n", nome, idade);
+    printf("obrigado por me informar seu nome e idade\n");
+    printf ("me fala aonde você mora?\n");
+    scanf("%s", nome);
+    printf("obrigado por me informar onde você mora\n");
+
 
     return 0;
+
+
+
+
+
 }
